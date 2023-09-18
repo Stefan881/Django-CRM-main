c4bd77e38d9a90ff2d67dbfb6c92494fd7f8d484
Ondeyo Stephen <ondeyostephen0@gmail.com>
2023-09-18 14:21:02 +0300

    tweaked app for heroku

--- dcrm/settings.py
+++ dcrm/settings.py
@@ -1,5 +1,8 @@
 from pathlib import Path
-
+import os
+import django_heroku 
+import dj_database_url
+from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -36,6 +39,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMidddleware',
 ]
 
 ROOT_URLCONF = 'dcrm.urls'
@@ -109,8 +113,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+django_heroku.settings(locals())
